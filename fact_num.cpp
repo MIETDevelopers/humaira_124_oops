2cd0005bbbe8a8102395a48993d1d2b544383797
Humaira Gani <80723718+humaira2020@users.noreply.github.com>
2022-06-11 12:43:17 +0530

    Add files via upload

---
+++ fact_num.cpp
@@ -0,0 +1,17 @@
+lude<stdio.h>
+int main(){
+	int i,fact=1,number;
+	scanf("%d,&number");
+	printf("Enter a number;%d,number");
+	//using for loop
+	for(i=1;i<=number;i++);
+	{
+	fact=fact*1;
+    }  
+ 
+        printf("\nfactori//To calculate factorial of a number
+#incal of %d is;%d,number,fact");
+         return 0;
+  
+	
+        }
